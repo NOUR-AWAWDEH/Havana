ec65f6ad96f847fa8521a09f6b7c9b5a87dceb9b
NOUR-AWAWDEH <nour.awawdeh95@gmail.com>
2024-04-30 02:37:16 +0300

    Re Desgine the NewOrderWindow

--- Havana/Orders/NewOrderWindow.xaml.cs
+++ Havana/Orders/NewOrderWindow.xaml.cs
@@ -19,9 +19,102 @@ namespace Havana.Orders
 
         public NewOrderWindow()
         {
-            InitializeComponent();
-            
-            
+            InitializeComponent();           
+        }
+
+        public void OpenWindow(Type windowType)
+        {
+            Window window = (Window)Activator.CreateInstance(windowType);
+            this.Visibility = Visibility.Visible;
+            window.Show();
+        }
+
+        private void DeleteSelectedItemButt_Click(object sender, RoutedEventArgs e)
+        {
+            if (OrderDataGrid.SelectedItem != null)
+            {
+                OrderDataGrid.Items.Remove(OrderDataGrid.SelectedItem);
+            }
+        }
+
+        private void OrderDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DeleteSelectedItemButt.Visibility = OrderDataGrid.SelectedItem != null ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        private void ItemsComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+            if (ItemsComboBox.SelectedValue != null)
+            {
+                if (ItemsComboBox.SelectedValue is Drink selectedDrink)
+                {
+                    OrderDataGrid.Items.Add(selectedDrink);
+                }
+                else if (ItemsComboBox.SelectedValue is Snack selectedSnack)
+                {
+                    OrderDataGrid.Items.Add(selectedSnack);
+                }
+            }
+        }
+
+        //ItemsButt
+        private void SnaksItemsButt(object sender, RoutedEventArgs e)
+        {
+            OpenWindow(typeof(SnacksItems));
+        }
+
+        private void DrinksItemsButt(object sender, RoutedEventArgs e)
+        {
+            OpenWindow(typeof(DrinksItems));
+        }
+
+        //Buttons
+        public void BuyerNameButt(object sender, RoutedEventArgs e)
+        {
+            DataAccess dataAcces = new DataAccess();
+
+            string name = BuyerNameTextBox.Text;
+            if (!string.IsNullOrWhiteSpace(name))
+            {
+                buyer.Name = name;
+                dataAcces.InsertBuyerName(buyer);
+                NewOrderWindow newOrderWindow = Application.Current.Windows.OfType<NewOrderWindow>().FirstOrDefault();
+                newOrderWindow.Show();
+
+            }
+        }
+
+        private void OrderButt(object sender, RoutedEventArgs e)
+        {
+
+        }
+        
+        private void SearchButt(object sender, RoutedEventArgs e)
+        {
+            Drinks = dataAccess.GetDrinks();
+            ItemsComboBox.Items.Clear();
+
+            string searchTerm = SearchTextBox.Text.ToLower();
+
+            foreach (Drink drink in Drinks)
+            {
+                if (drink.Name.ToLower().StartsWith(searchTerm) || drink.Cost.ToString().ToLower().StartsWith(searchTerm))
+                {
+                    ItemsComboBox.Items.Add(drink);
+                     
+                           
+                }
+            }
+
+            Snacks = dataAccess.GetSnacks();
+            foreach (Snack snack in Snacks)
+            {
+                if (snack.Name.ToLower().StartsWith(searchTerm) || snack.Cost.ToString().ToLower().StartsWith(searchTerm))
+                {
+                    ItemsComboBox.Items.Add(snack);                  
+                }
+            }
         }
 
         private void BackToHavana(object sender, RoutedEventArgs e)
@@ -50,92 +143,5 @@ namespace Havana.Orders
             this.Show();
         }
 
-        public void OpenWindow(Type windowType)
-        {
-            Window window = (Window)Activator.CreateInstance(windowType);
-            this.Visibility = Visibility.Visible;
-            window.Show();
-        }
-
-        private void SnaksItemsButt(object sender, RoutedEventArgs e)
-        {
-            OpenWindow(typeof(SnacksItems));
-        }
-
-        private void DrinksItemsButt(object sender, RoutedEventArgs e)
-        {
-            OpenWindow(typeof(DrinksItems));
-        }
-
-        private void SearchButt(object sender, RoutedEventArgs e)
-        {
-            Drinks = dataAccess.GetDrinks();
-            ItemsComboBox.Items.Clear();
-
-            string searchTerm = SearchTextBox.Text.ToLower();
-
-            foreach (Drink drink in Drinks)
-            {
-                if (drink.Name.ToLower().StartsWith(searchTerm) || drink.Cost.ToString().ToLower().StartsWith(searchTerm))
-                {
-                    ItemsComboBox.Items.Add(drink);
-                     
-                           
-                }
-            }
-
-            Snacks = dataAccess.GetSnacks();
-            foreach (Snack snack in Snacks)
-            {
-                if (snack.Name.ToLower().StartsWith(searchTerm) || snack.Cost.ToString().ToLower().StartsWith(searchTerm))
-                {
-                    ItemsComboBox.Items.Add(snack);                  
-                }
-            }
-        }
-
-        private void DeleteSelectedItemButt_Click(object sender, RoutedEventArgs e)
-        {
-            if (OrderDataGrid.SelectedItem != null)
-            {
-                OrderDataGrid.Items.Remove(OrderDataGrid.SelectedItem);
-            }
-        }
-
-        private void OrderDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            DeleteSelectedItemButt.Visibility = OrderDataGrid.SelectedItem != null ? Visibility.Visible : Visibility.Collapsed;
-        }
-
-        private void ItemsComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-           
-            if (ItemsComboBox.SelectedValue != null)
-            {
-                if (ItemsComboBox.SelectedValue is Drink selectedDrink)
-                {
-                    OrderDataGrid.Items.Add(selectedDrink);
-                }
-                else if (ItemsComboBox.SelectedValue is Snack selectedSnack)
-                {
-                    OrderDataGrid.Items.Add(selectedSnack);
-                }
-            }
-        }
-
-        public void BuyerNameButt(object sender, RoutedEventArgs e)
-        {
-            DataAccess dataAcces = new DataAccess();
-            
-            string name = BuyerNameTextBox.Text;
-            if (!string.IsNullOrWhiteSpace(name))
-            {
-                buyer.Name = name;
-                dataAcces.InsertBuyerName(buyer);
-                NewOrderWindow newOrderWindow = Application.Current.Windows.OfType<NewOrderWindow>().FirstOrDefault();
-                newOrderWindow.Show();
-
-            }
-        }
     }
 }

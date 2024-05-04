34411bc2aee8ccd9213733d8e9478f8342b60078
NOUR-AWAWDEH <nour.awawdeh95@gmail.com>
2024-05-04 13:20:08 +0300

    Fix the Bill Styling Output.

--- Library/Models/Classes/ReportGenerator.cs
+++ Library/Models/Classes/ReportGenerator.cs
@@ -28,6 +28,7 @@ namespace Library.Models.Classes
             Directory.CreateDirectory(folderPath);
 
             string currencySymbol = "BYN ";
+            int itemNameColumnWidth = 70;
             try
             {
                 string fileName = Path.Combine(folderPath, $"Bill_{order.DateTime:yyyyMMdd_HHmmss}.txt");
@@ -38,35 +39,29 @@ namespace Library.Models.Classes
 
                 using (StreamWriter sw = File.CreateText(fileName))
                 {
-                    // Assuming sw is the output stream or writer
+                    sw.WriteLine("".PadRight(87, '-'));
+                    sw.WriteLine($"{"".PadRight(24)}Havana".PadRight(45));
 
-                    // Calculate the maximum width of the snack names
-                    int maxWidth = 60;
-                    
-
-                    sw.WriteLine("".PadRight(80, '-'));
-                    sw.WriteLine($"{"".PadRight(34)}Havana");
-                    sw.WriteLine($"{"".PadRight(24)}Address 99,99 , City Gomel");
-                    sw.WriteLine($"{"".PadRight(26)}Phone: +37525-222-2222");
-                    sw.WriteLine("".PadRight(80, '-'));
-
-                    sw.WriteLine($"Date: {DateTime.Now.ToString("M/d/yyyy h:mm:ss tt")}");
+                    sw.WriteLine($"{"".PadRight(14)}Address 99,99 , City Gomel");
+                    sw.WriteLine($"{"".PadRight(16)}Phone: +37525-222-2222");
+                    sw.WriteLine("".PadRight(87, '-'));
+                    sw.WriteLine($"DateTime :  {order.DateTime.ToString("M/d/yyyy h:mm:ss tt")}");
                     sw.WriteLine($"Buyer Name: {order.BuyerName.Name}");
-                    sw.WriteLine("".PadRight(80, '-'));
-
-                    sw.WriteLine("Items".PadRight(60, ' ') + "Price".PadRight(12,' ') + "Count");
-                    sw.WriteLine("".PadRight(80, '-'));
-
+                    sw.WriteLine("".PadRight(87, '-'));
+                    sw.WriteLine("Items".PadRight(itemNameColumnWidth, ' ') + "Price".PadRight(12, ' ') + "Count");
+                    sw.WriteLine("".PadRight(87, '-'));
                     if (order.DrinksList.Drinks != null && order.DrinksList.Drinks.Any())
                     {
+
                         foreach (Drink drink in order.DrinksList.Drinks)
                         {
                             string itemLine = $"{drink.Name.ToLower()}";
-                            int remainingSpace = maxWidth - itemLine.Length;
-                            string priceLine = $"{currencySymbol}{drink.Cost.ToString("G29")}".PadRight(12, ' ');
-                            string countOfItems = $"{order.DrinksList.Count}";
-                            sw.WriteLine($"{itemLine}{new string(' ', remainingSpace)}{priceLine}{new string(' ', 8)}{countOfItems}");
+                            int remainingSpace = itemNameColumnWidth - itemLine.Length;
+                            string priceLine = $"{new string(' ', remainingSpace)}{currencySymbol}{drink.Cost.ToString("0.0#")}";
+                            string countOfItems = $"".PadRight(5, ' ') + $"{order.DrinksList.Count}";
+                            sw.WriteLine($"{itemLine}{priceLine}{countOfItems}");
                         }
+
                     }
 
                     if (order.SnacksList.Snacks != null && order.SnacksList.Snacks.Any())
@@ -74,16 +69,16 @@ namespace Library.Models.Classes
                         foreach (Snack snack in order.SnacksList.Snacks)
                         {
                             string itemLine = $"{snack.Name.ToLower()}";
-                            int remainingSpace = maxWidth - itemLine.Length;
-                            string priceLine = $"{currencySymbol}{snack.Cost.ToString("G29")}".PadRight(12);
-                            string countOfItems = $"{order.SnacksList.Count}";
-                            sw.WriteLine($"{itemLine}{new string(' ', remainingSpace)}{priceLine}{new string(' ', 8)}{countOfItems}");
+                            int remainingSpace = itemNameColumnWidth - itemLine.Length;
+                            string priceLine = $"{new string(' ', remainingSpace)}{currencySymbol}{snack.Cost.ToString("0.0#")}";
+                            string countOfItems = $"".PadRight(5, ' ') + $"{order.SnacksList.Count}";
+                            sw.WriteLine($"{itemLine}{priceLine}{countOfItems}");
                         }
                     }
 
-                    sw.WriteLine("".PadRight(80, '-'));
-                    sw.WriteLine("Total:".PadRight(maxWidth) + $"{currencySymbol}{order.TotalCost.ToString("0.##")}");
-                    sw.WriteLine("".PadRight(80, '-'));
+                    sw.WriteLine("".PadRight(87, '-'));
+                    sw.WriteLine($"Total:".PadRight(itemNameColumnWidth) + $"{currencySymbol}{order.TotalCost.ToString("0.##")}");
+                    sw.WriteLine("".PadRight(87, '-'));
                 }
             }
             catch (Exception ex)
@@ -92,51 +87,6 @@ namespace Library.Models.Classes
             }
         }
 
-        private string CenterText(string text)
-        {
-            int totalWidth = 82; // Total width of the line
-            int textWidth = text.Length;
-            int leftIndent = (totalWidth - textWidth) / 2;
-            string centeredText = text.PadLeft(leftIndent + textWidth).PadRight(totalWidth);
-            return centeredText;
-        }
-
-        
-        public void SummaryReport(List<Order> orderList)
-        {
-            string folderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "Bills");
-            Directory.CreateDirectory(folderPath);
-
-            foreach (var order in orderList)
-            {
-                string fileName = Path.Combine(folderPath, $"Bill_{order.DateTime:yyyyMMdd_HHmmss}.txt");
-
-                try
-                {
-                    using (StreamWriter sw = File.CreateText(fileName))
-                    {
-                        // Write the order details to the file
-                        sw.WriteLine($"Order Date: {order.DateTime}");
-                        sw.WriteLine($"Buyer Name: {order.BuyerName}");
-                        sw.WriteLine("Drinks:");
-                        foreach (var drink in order.DrinksList.Drinks)
-                        {
-                            sw.WriteLine($"{drink.Name} - {drink.Cost}");
-                        }
-                        sw.WriteLine("Snacks:");
-                        foreach (var snack in order.SnacksList.Snacks)
-                        {
-                            sw.WriteLine($"{snack.Name} - {snack.Cost}");
-                        }
-                    }
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-                }
-            }
-        }
-
-
+       
     }
 }
